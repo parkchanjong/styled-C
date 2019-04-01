37b9280ef405e69542796210c527c2d438a40389
parkchanjong <7000cj@gmail.com>
2019-04-01 12:14:16 +0900

    stlye-c

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class App extends Component {
 	render() {
 		return (
-			<Fragment>
-				<Button danger />
-				<Button />
-			</Fragment>
+			<Container>
+				<Button>Hello</Button>
+				<Button danger>Hello</Button>
+			</Container>
 		);
 	}
 }
+const Container = styled.div`
+	height: 100vh;
+	width: 100%;
+	background-color: pink;
+`;
 
-const Button = ({ danger }) => (
-	<button className={danger ? 'button button--success ' : 'button button--danger'}>hi</button>
-);
+const Button = styled.button`
+	border-radius: 50px;
+	padding: 5px;
+	min-width: 120px;
+	color: white;
+	font-weight: 600;
+	-webkit-appearance: none;
+	cursor: pointer;
+	&:active,
+	&:focus {
+		outline: none;
+	}
+	background-color: ${props => (props.danger ? '#e74c3c' : '#2ecc71')};
+`;
 
 export default App;
